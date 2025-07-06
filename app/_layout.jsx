6a0a16e1492261c766790fa2b4d7e295717bf115
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-07-06 22:32:52 +0500

    resolve(karim) : Merge Conflicts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-import { useRouter } from "expo-router";
-import React, { useEffect, useState } from "react";
-
-import { View, ActivityIndicator } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import supabase from "../lib/supabase";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-=======
-
->>>>>>> b8289fd400bd88f0ce80399fbb0ab7e8918701b2
 
 import { Stack, SplashScreen } from "expo-router";
 import { useFonts } from "expo-font";
 import "../global.css";
-<<<<<<< HEAD
-=======
-import React, { useEffect } from "react";
-import supabase from "../lib/supabase";
->>>>>>> b8289fd400bd88f0ce80399fbb0ab7e8918701b2
 
 export default function _Layout() {
   SplashScreen.preventAutoHideAsync();
@@ -29,12 +12,6 @@ export default function _Layout() {
     "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
   });
 
-<<<<<<< HEAD
-  const [onboardingChecked, setOnboardingChecked] = useState(false);
-  const router = useRouter();
-
-=======
->>>>>>> b8289fd400bd88f0ce80399fbb0ab7e8918701b2
   useEffect(() => {
     if (error) throw error;
     if (fontsloaded) {
@@ -45,7 +22,6 @@ export default function _Layout() {
   if (!fontsloaded && !error) {
     return null;
   }
-<<<<<<< HEAD
 
   return (
     <SafeAreaProvider>
@@ -57,18 +33,4 @@ export default function _Layout() {
       </Stack>
     </SafeAreaProvider>
   );
-=======
-  
-  supabase.auth.signOut();
-  return (
-    
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
- 
-  );
-  
->>>>>>> b8289fd400bd88f0ce80399fbb0ab7e8918701b2
 }
